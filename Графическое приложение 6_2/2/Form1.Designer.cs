b27ebf826b7a51529803ce365e0858e0d60f6343
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-09 21:37:09 +0300

    Add files via upload

---
+++ Графическое приложение 6_2/2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.n_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mas_textBox = new System.Windows.Forms.RichTextBox();
+            this.res_textBox = new System.Windows.Forms.RichTextBox();
+            this.res_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(31, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите разрядность массива: ";
+            // 
+            // n_textBox
+            // 
+            this.n_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.n_textBox.Location = new System.Drawing.Point(357, 28);
+            this.n_textBox.Name = "n_textBox";
+            this.n_textBox.Size = new System.Drawing.Size(147, 30);
+            this.n_textBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(31, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Массив: ";
+            // 
+            // mas_textBox
+            // 
+            this.mas_textBox.Location = new System.Drawing.Point(131, 86);
+            this.mas_textBox.Name = "mas_textBox";
+            this.mas_textBox.Size = new System.Drawing.Size(373, 103);
+            this.mas_textBox.TabIndex = 7;
+            this.mas_textBox.Text = "";
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Location = new System.Drawing.Point(24, 282);
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.Size = new System.Drawing.Size(505, 136);
+            this.res_textBox.TabIndex = 13;
+            this.res_textBox.Text = "";
+            // 
+            // res_button
+            // 
+            this.res_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res_button.Location = new System.Drawing.Point(192, 207);
+            this.res_button.Name = "res_button";
+            this.res_button.Size = new System.Drawing.Size(161, 40);
+            this.res_button.TabIndex = 12;
+            this.res_button.Text = "Вычислить";
+            this.res_button.UseVisualStyleBackColor = true;
+            this.res_button.Click += new System.EventHandler(this.res_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 450);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.res_button);
+            this.Controls.Add(this.mas_textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.n_textBox);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Поиск минимального";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox n_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox mas_textBox;
+        private System.Windows.Forms.RichTextBox res_textBox;
+        private System.Windows.Forms.Button res_button;
+    }
+}
+
---
+++ Графическое приложение 6_2/2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        private int n;
+        private double[] arr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double Min(double[] mas)
+        {
+            double min = mas[0], mi = 0;
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (min > mas[i])
+                {
+                    min = mas[i];
+                    mi = i;
+                }
+            }
+            return mi;
+        }
+
+        private void res_button_Click(object sender, EventArgs e)
+        {
+            if (n_textBox.Text == "")
+            {
+                MessageBox.Show("Введены не все данные!!!");
+                return;
+            }
+            else
+            {
+                res_textBox.Text = "";
+                if (!int.TryParse(n_textBox.Text, out n) || n <= 0)
+                {
+                    MessageBox.Show("В поле N введены недопустимые данные! Введите целое число больше 0!");
+                    return;
+                }
+                arr = new double[n];
+                try
+                {
+                    var digits = mas_textBox.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (digits.Length > n)
+                    {
+                        MessageBox.Show("Число введенных элементов больше, чем количество элементов массива!");
+                        return;
+                    }
+                    else if (digits.Length < n)
+                    {
+                       MessageBox.Show("Число введенных элементов меньше, чем количество элементов массива!");
+                        return;
+                    }
+
+                    res_textBox.Text += "Изначальный массив: ";
+                    for (int i = 0; i < n; i++)
+                    {
+                        arr[i] = double.Parse(digits[i]);
+                        res_textBox.Text += arr[i] + " ";
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("В поле элементов массива введены неверные значения! Введите заново, используя только цифры, точку и знак \"минус\"");
+                    return;
+                }
+                res_textBox.Text += "\nНомер первого минимального элемента: ";
+                double min = arr[0], mi = 0;
+                for (int i = 1; i <arr.Length; i++)
+                {
+                    if (min > arr[i])
+                    {
+                        min = arr[i];
+                        mi = i;
+                    }
+                }
+                res_textBox.Text += Math.Round(mi, 1);
+            }
+        }
+    }
+}
